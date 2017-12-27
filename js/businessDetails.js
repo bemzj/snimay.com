abe380ef2e9371b1c342c0b4bdd9dec09177999f
Your Name <email@example.com>
2017-12-28 00:12:48 +0800

    1228

---
+++ js/businessDetails.js
@@ -0,0 +1,10 @@
+$(function(){
+	var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop:true,
+        prevButton:'.prev',
+		nextButton:'.next',
+    })
+});

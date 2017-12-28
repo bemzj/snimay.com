57e6f2600d685b6b569c55ff428acc03674ef583
bemzj <qiuzijia520@qq.com>
2017-12-28 11:53:23 +0800

    xx

--- js/businessDetails.js
+++ js/businessDetails.js
@@ -7,8 +7,19 @@ $(function(){
         prevButton:'.prev',
 		nextButton:'.next',
     });
+    //选择颜色
     $('.colorList').find('a').click(function(){
     	$(this).addClass('colorActive');
     	$(this).parent('li').siblings('li').find('a').removeClass('colorActive');
+    });
+    //选择实木类型
+    $('.treeList').find('a').click(function(){
+    	$(this).addClass('treeActive');
+    	$(this).parent('li').siblings('li').find('a').removeClass('treeActive');
+    });
+    //类型选择
+    $('.styleList').find('a').click(function(){
+    	$(this).addClass('classifyActive');
+    	$(this).parent('li').siblings('li').find('a').removeClass('classifyActive');
     });
 });
